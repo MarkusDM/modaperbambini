49d39d3c6fc5d953067fe1f9fdd5aa75ac04af26
Dmitry <mazarini200@yandex.ru>
2024-09-02 15:47:58 +0500

    update 2

--- src/js/dev/markusDM.js
+++ src/js/dev/markusDM.js
@@ -105,3 +105,31 @@ $(document).ready(function() {
     });
 });
 
+
+$('.header__select').on('click', '.header__select-head', function () {
+    if ($(this).hasClass('open')) {
+        $(this).removeClass('open');
+        $(this).next().fadeOut();
+    } else {
+        $('.header__select-head').removeClass('open');
+        $('.header__select-list').fadeOut();
+        $(this).addClass('open');
+        $(this).next().fadeIn();
+    }
+});
+
+$('.header__select').on('click', '.header__select-item', function () {
+    $('.header__select-head').removeClass('open');
+    $(this).parent().fadeOut();
+    $(this).parent().prev().text($(this).text());
+    $(this).parent().prev().prev().val($(this).text());
+});
+
+$(document).click(function (e) {
+    if (!$(e.target).closest('.header__select').length) {
+        $('.header__select-head').removeClass('open');
+        $('.header__select-list').fadeOut();
+    }
+});
+
+
